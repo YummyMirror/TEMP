c4f87cc876ba62cd0278d0be0ae78151a5b1ad7d
YummyMirror <anatolianiukevich@gmail.com>
2019-02-23 17:26:02 +0300

    Have added 'Test1' class

---
+++ project-1/src/test/java/Test1.java
@@ -0,0 +1,8 @@
+public class Test1 {
+    private static String a = "a";
+    private static String b = "b";
+
+    public static void main(String[] args) {
+        System.out.println(a + b);
+    }
+}
