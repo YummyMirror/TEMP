2cfdcb7f4dc1806a7355d5db05c7b69c29bba2b7
YummyMirror <anatolianiukevich@gmail.com>
2019-02-23 20:42:25 +0300

    Add method 'concat' to 'Feature3' class

--- project-1/src/test/java/Feature3.java
+++ project-1/src/test/java/Feature3.java
@@ -1,8 +1,16 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Feature3 {
     private static final String PATH = "Some path";
 
     public static void main(String[] args) {
         System.out.println(PATH);
         System.out.println("Path is: " + PATH);
+    }
+
+    private static String concat(List<String> list) {
+        return list.stream()
+                .collect(Collectors.joining());
     }
 }
