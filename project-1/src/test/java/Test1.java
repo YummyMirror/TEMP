b833c9f88b6498baa5fad8a7846e4413a2c91d52
YummyMirror <anatolianiukevich@gmail.com>
2019-02-23 19:20:53 +0300

    Update Test1.java
    Add Strings concatenation

--- project-1/src/test/java/Test1.java
+++ project-1/src/test/java/Test1.java
@@ -1,12 +1,23 @@
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Test1 {
     private static String a = "a";
     private static String b = "b";
 
     public static void main(String[] args) {
-        System.out.println(a + b);
-        System.out.println(a + b);
-        System.out.println(a + b);
-        System.out.println(a + b);
-        System.out.println(a + b);
+        System.out.println(stringConcatenation(a, b, "yeah"));
+    }
+
+    private static String stringConcatenation(String... strings) {
+        Objects.requireNonNull(strings);
+        String result = "";
+        if (strings.length > 0) {
+            StringBuilder builder = new StringBuilder();
+            Arrays.stream(strings)
+                    .forEach(builder::append);
+            result = builder.toString();
+        }
+        return result;
     }
 }
